f43a743aef283b7e8180d612cfb76f64e904ca2a
Louse0x <82267923+louse0x@users.noreply.github.com>
2021-09-23 14:31:06 +0800

    ""

--- module/jssl.py
+++ module/jssl.py
@@ -3,10 +3,12 @@
 """
 :desc: ssl subdomain
 """
+import re
 from urllib import request
 from tldextract import tldextract
 
 from module.header import header
+from bs4 import BeautifulSoup
 
 
 def jssl(domain):
@@ -15,13 +17,20 @@ def jssl(domain):
     domain = ext.domain + '.' + ext.suffix
 
     try:
+        data = dict()
+        # jssl_1
         req = request.Request('https://crt.sh/?q={0}'.format(domain), headers={'User-Agent': header()})
         res = request.urlopen(req).read().decode('utf-8')
-        print(res)
-
+        soup = BeautifulSoup(res, 'lxml')
+        td_list = [x for x in soup.find_all(text=re.compile(domain)) if ' ' not in x]
+        data['jssl_1'] = set(td_list)
+        return data
 
     except Exception as e:
+        # TODO:: LOG ERROR
+        print(e)
+        return -1
         pass
 
 
-print(j_ssl('www.tjhzyl.com'))
+print(jssl('www.tjhzyl.com'))
